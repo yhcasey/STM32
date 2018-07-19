221a4451e67cb879819fff41cf1bd3bbd2701eb7
casey <yhcasey@163.com>
2018-07-19 19:11:42 +0800

    add file

--- stm32_project_01/USER/main.c
+++ stm32_project_01/USER/main.c
@@ -4,21 +4,16 @@
 #include "led.h"
 #include "includes.h"
 #include "os_app_hooks.h"
-//ALIENTEK 探索者STM32F407开发板 UCOSIII实验
-//例4-1 UCOSIII UCOSIII移植
 
-//UCOSIII中以下优先级用户程序不能使用，ALIENTEK
-//将这些优先级分配给了UCOSIII的5个系统内部任务
-//优先级0：中断服务服务管理任务 OS_IntQTask()
-//优先级1：时钟节拍任务 OS_TickTask()
-//优先级2：定时任务 OS_TmrTask()
-//优先级OS_CFG_PRIO_MAX-2：统计任务 OS_StatTask()
-//优先级OS_CFG_PRIO_MAX-1：空闲任务 OS_IdleTask()
-//技术支持：www.openedv.com
-//淘宝店铺：http://eboard.taobao.com  
-//广州市星翼电子科技有限公司  
-//作者：正点原子 @ALIENTEK
-
+/*
+* UCOSIII中以下优先级用户程序不能使用
+* 将这些优先级分配给了UCOSIII的5个系统内部任务
+* 优先级0：中断服务服务管理任务 OS_IntQTask()
+* 优先级1：时钟节拍任务 OS_TickTask()
+* 优先级2：定时任务 OS_TmrTask()
+* 优先级OS_CFG_PRIO_MAX-2：统计任务 OS_StatTask()
+* 优先级OS_CFG_PRIO_MAX-1：空闲任务 OS_IdleTask()
+*/
 //任务优先级
 #define START_TASK_PRIO		3
 //任务堆栈大小	
